0336696752a8efc54ee979693b3f0b65600fc13f
amaldethan <amaldethan@gmail.com>
2017-04-01 22:54:21 +0530

    #01042017

--- testdetails.php
+++ testdetails.php
@@ -1,7 +1,68 @@
 <?php
 session_start();
 include('dbconfig.php');
+
+require('razorpay/config.php');
+require('razorpay/razorpay-php/Razorpay.php');
+
+// Create the Razorpay Order
+
+use Razorpay\Api\Api;
+
+$api = new Api($keyId, $keySecret);
+
+//
+// We create an razorpay order using orders api
+// Docs: https://docs.razorpay.com/docs/orders
+//
+$orderData = [
+    'receipt'         => 3456,
+    'amount'          => 500 * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$displayAmount = $amount = $orderData['amount'];
+
+if ($displayCurrency !== 'INR')
+{
+    $url = "https://api.fixer.io/latest?symbols=$displayCurrency&base=INR";
+    $exchange = json_decode(file_get_contents($url), true);
+
+    $displayAmount = $exchange['rates'][$displayCurrency] * $amount / 100;
+}
+
+$data = [
+    "key"               => $keyId,
+    "amount"            => $amount,
+    "name"              => "Be A Whiz",
+    "description"       => "Exam Portal",
+    "image"             => "https://s29.postimg.org/r6dj1g85z/daft_punk.jpg",
+    
+    
+    "theme"             => [
+    "color"             => "#F37254"
+    ],
+    "order_id"          => $razorpayOrderId,
+];
+
+if ($displayCurrency !== 'INR')
+{
+    $data['display_currency']  = $displayCurrency;
+    $data['display_amount']    = $displayAmount;
+}
+
+$json = json_encode($data);
+
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -124,40 +185,38 @@ include('dbconfig.php');
 
 </nav>
 
-<div>
-	<?php 
+<div class="w3-row">
 
-		$query = mysqli_query($conn, 'SELECT * from tpack');
+	<?php
+
+	$id = $_GET['id']; 
+
+		$query = mysqli_query($conn, 'SELECT * from tpack WHERE id = "'.$id.'"');
 		while($res = mysqli_fetch_array($query)){
 			$name  = $res['pack_name'];
 			$subject = $res['subject'];
 			$syllabus = $res['syllabus'];
 			$grade = $res['grade'];
 		
-
+		}
 	?>
-	<div class="col-sm-4">
-		<div class="w3-card-4">
-			<header class="w3-container w3-light-grey">
-	  			<h3><?php echo $name; ?></h3>
-			</header>
-			<div class="w3-container" style="margin-top:10px;">
-			  <p><b>Subject : <?php echo $subject; ?></b></p>
-			  <hr>
-			  <p>Class : <?php echo $grade; ?></p>
-			  <hr>
-			  <p>Syllabus : <?php echo $syllabus; ?> </p>
-			</div>
-			<a role="button" href="testdetails.php?id=<?php echo $res['id']; ?>" class="w3-button w3-block w3-green w3-hover-blue">Read More</a>
-		</div>
+
+	<div class="w3-col-s6" style="width:50%;">
+	<div class="w3-container">
+  	<h3><?php echo $name; ?></h3>
+  	<hr>
+  	<h5>Syllabus : <?php echo $syllabus; ?></h5>
+  	<hr>
+  	<h5>Class : <?php echo $grade; ?></h5>
+  	<hr>
+  	<p>Description about the test</p>
+  	<a id="rzp-button1" role="button" href="razorpay/pay.php" class="w3-button w3-blue w3-hover-green" style="width:100%;">BUY</a>	
 	</div>
-
-	<?php } ?>
-
+	
+	</div>
 </div>
 
 </div>
-
 <footer class="mainfoot">
 	<div class="container-fluid">
 		<div class="row">
@@ -189,4 +248,45 @@ include('dbconfig.php');
 
 </footer>
 </body>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<form name='razorpayform' action="razorpay/verify.php" method="POST">
+    <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
+    <input type="hidden" name="razorpay_signature"  id="razorpay_signature" >
+</form>
+<script>
+// Checkout details as a json
+var options = <?php echo $json?>;
+
+/**
+ * The entire list of Checkout fields is available at
+ * https://docs.razorpay.com/docs/checkout-form#checkout-fields
+ */
+options.handler = function (response){
+    document.getElementById('razorpay_payment_id').value = response.razorpay_payment_id;
+    document.getElementById('razorpay_signature').value = response.razorpay_signature;
+    document.razorpayform.submit();
+};
+
+// Boolean whether to show image inside a white frame. (default: true)
+options.theme.image_padding = false;
+
+options.modal = {
+    ondismiss: function() {
+        console.log("This code runs when the popup is closed");
+    },
+    // Boolean indicating whether pressing escape key 
+    // should close the checkout form. (default: true)
+    escape: true,
+    // Boolean indicating whether clicking translucent blank
+    // space outside checkout form should close the form. (default: false)
+    backdropclose: false
+};
+
+var rzp = new Razorpay(options);
+
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp.open();
+    e.preventDefault();
+}
+</script>
 </html>
